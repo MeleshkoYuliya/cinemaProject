ac8639a8c2d12b9c166592fc91b7ec689a0ec9fe
Julia <yulia_lang@mail.ru>
2018-07-26 13:02:35 +0300

    refactoring

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import Header from "./components/header/Header";
+import "./App.css";
+import "./scss/style.scss";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+      </div>
+    );
+  }
+}
+
+export default App;
