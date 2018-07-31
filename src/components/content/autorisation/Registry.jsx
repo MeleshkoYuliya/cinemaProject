74fae5376243456aa96e9281ac83729064b9a410
Julia <yulia_lang@mail.ru>
2018-07-31 14:48:01 +0300

    change registry page

--- src/components/content/autorisation/Registry.jsx
+++ src/components/content/autorisation/Registry.jsx
@@ -1,16 +1,27 @@
 import React from "react";
 
 class Registry extends React.Component {
+  state = {
+    email: ""
+  };
+  handleSubmit = () => {
+    console.log("form is submitted");
+  };
+  handleEmailChange = () => {
+    console.log("email was changed", this);
+  };
   render() {
     return (
-      <form className="register-block">
+      <form className="register-block" onSubmit={this.handleSubmit}>
         <h4 className="login-join__title">Register</h4>
         <label className="login-join__info">
           <input
             className="login-join__add"
-            type="email"
+            type="text"
             name="email"
             placeholder="Email Address*"
+            value={this.state.email}
+            onChange={this.handleEmailChange}
           />
         </label>
         <label className="login-join__info">
