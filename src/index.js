dddebc0e449dacbc9773b74c3d088af33ebf1df6
Julia <yulia_lang@mail.ru>
2018-08-14 22:16:21 +0300

    d

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose} from "redux";
 import loginReducer from "./components/authorisation/reducers/login-reducer";
 import registryReducer from "./components/authorisation/reducers/registry-reducer";
 import reducer from "./components/mainLayout/reducers/reducer";
