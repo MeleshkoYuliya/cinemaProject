aa397adda1142acba0337139fde5fb71872fb3f6
Julia <yulia_lang@mail.ru>
2018-08-24 20:01:13 +0300

    merge and conflict

--- src/components/homePage/AboutFilm.jsx
+++ src/components/homePage/AboutFilm.jsx
@@ -1,45 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-import PropTypes from "prop-types";
-import { withRouter } from "react-router-dom";
-
-class AboutFilm extends React.Component {
-  static propTypes = {
-     film: PropTypes.shape({
-        id: PropTypes.number,
-        name: PropTypes.string,
-        url: PropTypes.string
-      })
-  };
-  render() {
-    console.log(this.props.film);
-    return (
-      <div>
-        <div className="session-selection">
-          <div>
-            {/* <div className="session-selection__film">
-              <h4 className="select-options__title">{this.props.film.name}</h4>
-              <img
-                className="selection__img"
-                src={this.props.film.url}
-                alt={this.props.film.name}
-              />
-            </div> */}
-
-            <div>Genre:</div>
-            <div>Age limit:</div>
-            <div> Director:</div>
-            <div>Actors:</div>
-            <div> Description:</div>
-            <div>
-              <button className="button session-selection__button">
-                View Treiler
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-=======
 import React from 'react';
 import PropTypes from 'prop-types';
 
@@ -77,14 +35,8 @@ class AboutFilm extends React.PureComponent {
          
             </div>
         </div>
->>>>>>> film
     );
   }
 }
 
-<<<<<<< HEAD
-export default withRouter(AboutFilm);
-=======
-
 export default AboutFilm;
->>>>>>> film
--- src/components/homePage/Films.jsx
+++ src/components/homePage/Films.jsx
@@ -1,85 +1,25 @@
 import React from "react";
 import PropTypes from "prop-types";
-<<<<<<< HEAD
-import { Switch, Route } from "react-router-dom";
-import AboutFilm from "./AboutFilm";
-=======
+
 import {  Route, NavLink } from "react-router-dom";
->>>>>>> film
+
 import { withRouter } from "react-router-dom";
 import AboutFilmPage from "./AboutFilmPage";
 
 class Films extends React.Component {
   static propTypes = {
-<<<<<<< HEAD
-    films: PropTypes.arrayOf(
-      PropTypes.shape({
-        id: PropTypes.number,
-        name: PropTypes.string,
-        url: PropTypes.string
-      })
-    )
-  };
-  state = {
-    film: {}
-  };
- 
-  handleSelectFilm = e => {
-    const { films } = this.props;
-    const { history } = this.props;
-    const movie = films.find(movie => movie.name === e);  
-    if (movie){ 
-      history.push(`/film/:${movie.id}`);
-      this.setState({
-        film: movie
-      });
-      console.log(this.state.film)
-    } 
-=======
+
+  
+
     info: PropTypes.shape({
     id: PropTypes.number,
     name: PropTypes.string,
     url: PropTypes.string,
   })
->>>>>>> film
+
   };
 
-  
   render() {
-<<<<<<< HEAD
- 
-    var filmsCode = this.props.films.map((film, index) => {
-      return (
-        <div className="home_film" key={index}>
-          <div className="film_title">
-            <h5>{film.name}</h5>
-          </div>
-          <img className="home__img" src={film.url} alt={film.name} />
-          <div>
-            {/* <button className="home__button">Treiler</button> */}
-            {/* <Link to="/buy-tickets"> */}
-            <button
-              className="home__button"
-              id={film.name}
-              onClick={() => this.handleSelectFilm(film.name)}
-            >
-              Tickets
-            </button>
-            {/* </Link> */}
-          </div>
-         
-          <Switch>
-            <Route path={`/film/:${film.id}`}>
-              <AboutFilm id={film.id} film={this.state.film} />
-            </Route>
-          </Switch>
-          {/* <Route path="/buy-tickets" component={BuyTickets} /> */}
-        </div>
-      );
-    });
-  return (<div className="home">{filmsCode}
-   </div>);
-=======
   
     return (
       <div className="home_film">
@@ -93,7 +33,6 @@ class Films extends React.Component {
         <Route path="/film/:id" component={AboutFilmPage}/>
       </div>
     );
->>>>>>> film
   }
 }
 
